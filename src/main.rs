c345142728a8b46f3462027a0d68c4781b0258f8
volodimir-pichkurov <vladimirpitbul@gmail.com>
2023-05-07 21:55:14 +0300

    app_data - AppState

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+// #[get("/")]
+// async fn hello() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world!")
+// }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -14,21 +14,36 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn index() -> impl Responder {
+async fn index0() -> impl Responder {
     "Some INDEX"
+}
+
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
             .service(
                 // prefixes all resources and routes attached to it...
                 web::scope("/app")
                     // ...so this handles requests for `GET /app/index.html`
-                    .route("/index.html", web::get().to(index)),
+                    .route("/index.html", web::get().to(index0)),
             )
-            .service(hello)
+            .service(index)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
