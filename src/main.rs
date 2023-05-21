9a4664993b85fcc2a813718c63082318c8a19ca9
volodimir-pichkurov <vladimirpitbul@gmail.com>
2023-05-21 11:21:38 +0300

    spa functional + api worked

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 mod api;
 
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use actix_web::{App, HttpServer};
+use actix_web_lab::web::spa;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
-            .service(hello)
             .service(api::api_scope())
+            .service(
+                spa()
+                    .index_file("./public/index.html")
+                    .static_resources_mount("/")
+                    .static_resources_location("./public")
+                    .finish()
+            )
     })
         .bind(("127.0.0.1", 8080))?
         .run()
